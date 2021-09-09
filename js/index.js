6a1afffffd586a63e6e43e1cf946225162f60a7c
Anshul Pratap Negi <60955293+anshulnegicr7@users.noreply.github.com>
2021-09-09 13:33:49 +0530

    Upload PORTFOLIO

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+const navToggle = document.querySelector('.nav_toggle');
+const navLinks = document.querySelectorAll('.nav_link')
+
+
+    navToggle.addEventListener('click', ()=> {
+    document.body.classList.toggle('nav_open');
+
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=> {
+        document.body.classList.remove('nav_open');
+    })
+})
